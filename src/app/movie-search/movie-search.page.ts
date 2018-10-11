0ccb42a717b4d4638fd60826972c8d2bc0e460ad
Herbert Fuchs <herbert.fuchs@student.tugraz.at>
2018-10-11 17:21:22 +0200

    added search

--- src/app/movie-search/movie-search.page.ts
+++ src/app/movie-search/movie-search.page.ts
@@ -1,15 +1,37 @@
-import { Component, OnInit } from '@angular/core';
+import {AfterViewChecked, AfterViewInit, Component, OnInit, ViewChild} from '@angular/core';
+import {ModalController, NavController, NavParams} from '@ionic/angular';
+import {Searchbar} from '@ionic/angular';
 
 @Component({
-  selector: 'app-movie-search',
-  templateUrl: './movie-search.page.html',
-  styleUrls: ['./movie-search.page.scss'],
+    selector: 'app-movie-search',
+    templateUrl: './movie-search.page.html',
+    styleUrls: ['./movie-search.page.scss'],
 })
-export class MovieSearchPage implements OnInit {
+export class MovieSearchPage implements OnInit, AfterViewInit {
+    search_query: string;
+    isToggled: boolean;
+    @ViewChild('searchbar') searchbar: Searchbar;
 
-  constructor() { }
 
-  ngOnInit() {
-  }
+    constructor(navCtrl: NavController, params: NavParams, public modalCtrl: ModalController) {
+        this.search_query = params.get('input');
+        this.isToggled = false;
+    }
 
+    ngOnInit() {
+    }
+
+    ngAfterViewInit() {
+        setTimeout(() => {
+            this.searchbar.focus();
+        }, 150);
+    }
+
+    closeModal() {
+        this.modalCtrl.dismiss();
+    }
+
+    notify() {
+
+    }
 }
